97a4ce3a120ed5b1480f7b63455f6e11272e2dae
Xavi2503 <168589871+Xavi2503@users.noreply.github.com>
2026-09-24 15:56:53 +0200

    Expose configured keyboard toggle to controller input

--- app/src/main/java/com/limelight/ui/GameGestures.java
+++ app/src/main/java/com/limelight/ui/GameGestures.java
@@ -5,6 +5,10 @@ import com.limelight.binding.input.GameInputDevice;
 public interface GameGestures {
     void toggleKeyboard();
 
+    default void toggleControllerKeyboard() {
+        toggleKeyboard();
+    }
+
     default void setPushToTalkPressed(boolean pressed){};
 
     default void showGameMenu(GameInputDevice device){};
